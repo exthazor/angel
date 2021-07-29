5e64b99df8a017cd5fa22cd7969f1b05ae53a56e
Rhitam <rhitamdutta1996@gmail.com>
2021-07-30 00:25:23 +0530

    Added message command

---
+++ src/commands/Moderation Commands/MessageCommand.ts
@@ -0,0 +1,48 @@
+import { Command } from "discord-akairo";
+import { Message, TextChannel } from "discord.js";
+import ms from "ms";
+import BotClient from "../../client/BotClient";
+
+export default class MessageCommand extends Command {
+  public constructor() {
+    super("message", {
+      aliases: ["message", "msg"],
+      category: "Moderation Commands",
+      ratelimit: 3,
+      userPermissions: ["ADMINISTRATOR"],
+      args: [
+        {
+          id: "channel_id",
+          type: "string",
+          prompt: {
+            start: (msg: Message) =>
+              `${"<@" + msg.author + ">"}, provide a channel id.`,
+            retry: (msg: Message) =>
+              `${"<@" + msg.author + ">"}, provide a valid channel id.`,
+          },
+        },
+        {
+          id: "msg",
+          type: "string",
+          match: "rest",
+          prompt: {
+            start: (msg: Message) =>
+              `${"<@" + msg.author + ">"}, provide a message for me to send.`,
+            retry: (msg: Message) =>
+              `${"<@" + msg.author + ">"}, provide a message for me to send.`,
+          },
+        },
+      ],
+    });
+  }
+
+  public async exec(
+    message: Message,
+    { channel_id, msg }: { channel_id: string; msg: string }
+  ): Promise<void> {
+    const channel = this.client.channels.cache.get(channel_id);
+    if (channel.isText()) {
+      (<TextChannel>channel).send(`${msg}`);
+    }
+  }
+}
