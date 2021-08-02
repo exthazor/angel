3ff082ba8908bc397f534e857e4dcff44374eb17
Rhitam Dutta <rhitamdutta1996@gmail.com>
2021-08-02 19:34:33 +0530

    Commented config file

--- src/structures/Database.ts
+++ src/structures/Database.ts
@@ -1,7 +1,6 @@
- 
 import { ConnectionManager } from "typeorm"
 import { Warns } from "../models/Warns"
-import { dbName } from "../config" 
+//import { dbName } from "../config" 
 import { Giveaways } from "../models/Giveaways"
 import { Replies } from "../models/Replies"
 import { Birthdays } from "../models/Birthdays"
@@ -9,13 +8,12 @@ import { GetBirthdays } from "../models/GetBirthdays"
 
 const connectionManager: ConnectionManager = new ConnectionManager()
 connectionManager.create({
-    name: dbName,
+    name: process.env.dbName,
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "entropy",
-    password: "kEYBOARDS7!",
-    database: "AngDB",
+    url: process.env.DATABASE_URL,
+    extra:{
+        ssl: true
+    },
     entities: [
         Warns,   //each of our database models
         Giveaways,
